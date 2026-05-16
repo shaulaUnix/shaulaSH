00e7d14b469b7edf20ad497699f28a6addaea365
shaulaUnix <mannyson@tutamail.com>
2026-05-16 13:33:05 +0800

    A script to initialize wgcf

---
+++ wgcf.sh
@@ -0,0 +1,17 @@
+# INSTALL WGCF IN TERMUX WITH ALPINE LINUX
+# ----------------------------------------
+# Prerequisites include Termux, PRoot-Distro, and wget
+# Termux version: 0.119.0 BETA
+# PRoot-Distro version: Pre-release
+
+# Update and upgrade the current packages
+apt update && yes | apt upgrade
+
+# Installing the requirements
+apt install -y python-pip proot-distro proot
+
+# Download the installer
+curl -O ...
+
+# Install Alpine Linux and then start it
+pd i alpine && pd sh alpine --termux-home
